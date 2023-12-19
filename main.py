15c3d38b0cf283456e3eefd84a96fd8229e154f0
CharlyCeballos <charlyfaceam912@gmail.com>
2023-12-19 16:14:51 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+speed(20)
+bgcolor("black")
+h = 0
+
+# Dibujar el girasol
+penup()
+goto(0, 0)
+pendown()
+for i in range(16):
+    for j in range(18):
+        color("yellow")  # Todos los pétalos son amarillos
+        h += 0.005
+        rt(90)
+        circle(150 - j * 6, 90)
+        lt(90)
+        circle(150 - j * 6, 90)
+        rt(180)
+    circle(40, 24)
+
+end_fill()
+done()
